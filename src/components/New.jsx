d75c6f078007f1d8c35766b117509f53c930bcb3
fran <frano4240>
2021-02-08 04:54:51 -0300

    bg image resize

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -5,7 +5,7 @@ const New = ({key,notice}) => {
     return (
         <div className="d-flex align-items-center pb-9">
                                 <div className="symbol symbol-70 symbol-2by3 flex-shrink-0 mr-6">
-                                            <div className="symbol-label" style={{height:'70px',width:'105px', backgroundImage: `url(${notice.imageurl})`}}></div>
+                                            <div className="symbol-label" style={{height:'70px',width:'105px', backgroundImage: `url(${notice.imageurl})`, backgroundSize:'cover'}}></div>
                                 </div>
                                 <div className="d-flex flex-column flex-grow-1">
                                     <a href={notice.url} className="text-dark-75 font-weight-bolder text-hover-primary font-size-lg mb-1">{notice.title}</a>
