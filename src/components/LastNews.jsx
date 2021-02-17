91880feb454496101f6ce2c7635fda8568ed8262
fran <frano4240>
2021-02-16 23:24:18 -0300

    keys

--- src/components/LastNews.jsx
+++ src/components/LastNews.jsx
@@ -18,7 +18,7 @@ const LastNews = () => {
         setIsLoading(true);
         const response = await Axios.get(`https://min-api.cryptocompare.com/data/v2/news/`,{params:{
             lang: 'ES',
-            api_key: '76078a1b07abf4d50a870169fb522c23c372527b112975146ecd8792cb6f5434',
+            api_key: process.env.API_KEY_CRYPTOCOMPARE,
         }})
             ;
         
