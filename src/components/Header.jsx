10253c253c84cb2718670ad15eca086d9b04f8aa
fran <frano4240>
2021-02-17 02:51:51 -0300

    icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <h1 className="text-center mt-3 mb-4 logo"><a href="/">tobitornottobit</a></h1>
+      <h1 className="text-center mt-3 mb-4 logo"><a href="/">ucanBit</a></h1>
       
     </div>
   );
